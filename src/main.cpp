112c12a3ee87e5d6a3b14427a5c784ed8270fb93
Caleb Smith <csmith@caktusgroup.com>
2012-01-19 11:26:33 -0500

    Improved style in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,27 @@
 #include <math.h>
 #include <new>
 
+#include <SDL.h> 
+#include <SDL_image.h>
+#include "SDL/SDL_ttf.h"
+
 #include "constants.h"
 #include "functions.h"
 #include "variables.h"
 #include "classes.h"
 
-#include <SDL.h> 
-#include <SDL_image.h>
-#include "SDL/SDL_ttf.h"
 
-int main(int argc, char *argv[])
-{   
+int main(int argc, char *argv[]) {
     init(images, fonts);   
-    while (gameopen){                                           // loop until x is clicked
+    while (gameopen) {
         loadmap(area, level, player1, images); mapstatic = 1;
-        // begin game loop
-        while (gameopen && mapstatic){                          // loop until x is clicked, or changing the map
-            gameopen = get_input(event, input);	                // get input, then do logic, then render graphics
-            mapstatic = logic(level, player1, camera, input);   
-            render(level, player1, camera, images, fonts, frametimer);   
-        }             
-    }    
-    quit(images, fonts);// free memory before closing
+        while (gameopen && mapstatic) {
+            gameopen = get_input(event, input);
+            mapstatic = logic(level, player1, camera, input);
+            render(level, player1, camera, images, fonts, frametimer);
+        }
+    }
+    quit(images, fonts);
     return 0;
 }
 
