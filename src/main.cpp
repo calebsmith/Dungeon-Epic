efbf9f8a94533120eb8d55451b9bba579c859afc
Caleb Smith <csmith@caktusgroup.com>
2012-01-19 10:59:46 -0500

    Moved src files into /src

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,16 @@
 int main(int argc, char *argv[])
 {   
     init(images, fonts);   
-    while (gameopen){                                           //loop until x is clicked
+    while (gameopen){                                           // loop until x is clicked
         loadmap(area, level, player1, images); mapstatic = 1;
-        //begin game loop
-        while (gameopen && mapstatic){                          //loop until x is clicked, or changing the map
-            //frametimer.reset();
-            gameopen = get_input(event, input);	                //get input, then do logic, then render graphics
+        // begin game loop
+        while (gameopen && mapstatic){                          // loop until x is clicked, or changing the map
+            gameopen = get_input(event, input);	                // get input, then do logic, then render graphics
             mapstatic = logic(level, player1, camera, input);   
             render(level, player1, camera, images, fonts, frametimer);   
         }             
     }    
-    quit(images, fonts);//free memory before closing
+    quit(images, fonts);// free memory before closing
     return 0;
 }
 
